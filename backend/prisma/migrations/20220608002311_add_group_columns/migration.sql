1917840581076677ec0d299db97a01410f1147b2
resende-gabriel <gabriel.rabh@gmail.com>
2022-06-07 21:27:28 -0300

    add group fields

---
+++ backend/prisma/migrations/20220608002311_add_group_columns/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "group" ADD COLUMN     "cep" INTEGER,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "complement" TEXT,
+ADD COLUMN     "description" TEXT,
+ADD COLUMN     "neighborhood" TEXT,
+ADD COLUMN     "state" TEXT,
+ADD COLUMN     "street" TEXT;
