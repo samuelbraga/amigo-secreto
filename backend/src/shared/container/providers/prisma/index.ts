44859225d70c970183e89ac1cd4f729226e4733a
Samuel Braga <samuel.fantini.braga@hotmail.com>
2022-07-04 14:28:04 -0300

    Criando setUp de teste de integração

--- backend/src/shared/container/providers/prisma/index.ts
+++ backend/src/shared/container/providers/prisma/index.ts
@@ -3,6 +3,9 @@ import { container } from "tsyringe";
 import { PRISMA_CLIENT } from "@constants/application";
 import { PrismaClient } from "@prisma/client";
 
+import prisma from "./implementation";
+import prismaMock from "./mock";
+
 container.register<PrismaClient>(PRISMA_CLIENT, {
-    useValue: new PrismaClient(),
+    useValue: process.env.NODE_ENV === "test" ? prismaMock : prisma,
 });
--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -1,35 +1,4 @@
-import "dotenv/config";
-import "reflect-metadata";
-
-import "@shared/container";
-
-import { errors } from "celebrate";
-import cors from "cors";
-import express from "express";
-import swaggerUi from "swagger-ui-express";
-
-import routes from "@shared/http/routes";
-
-import ErrorHandler from "./middleware/ErrorHandler";
-import swaggerFile from "./swagger.json";
-
-const app = express();
-app.disable("x-powered-by");
-
-const corsOptions = {
-    origin: ["http://localhost:3000", "http://www.mumuz.in"],
-};
-
-app.use(cors(corsOptions));
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use("/v1", routes);
-
-app.use(errors());
-app.use(ErrorHandler);
+import app from "./app";
 
 const { PORT } = process.env;
 app.listen(PORT || 3333, () => {
